7c61f0a0ae9495e21a0baf7569e4ebc4eca364b4
Sam Gehman <sgehman@cs.uw.edu>
2019-04-22 23:30:17 -0700

    Add more scraping code

--- aaec_scraper.py
+++ aaec_scraper.py
@@ -1,52 +1,68 @@
+# Example URL:
+# http://editorialcartoonists.com/cartoon/browse.cfm/?membertype=Regular
+# &cartoonist=Regular&datefrom=2018-01-01&orderby=publicationdate&sortorder=desc
+# &totalcount=0&submit=Search
+
 import requests
 from bs4 import BeautifulSoup
 import re
+from tqdm import tqdm
 
-# http://editorialcartoonists.com/cartoon/browse.cfm/?membertype=Regular&cartoonist=Regular&datefrom=2018-01-01&orderby=publicationdate&sortorder=desc&totalcount=0&submit=Search
+# 5 cartoon urls per search page
+_CARTOONS_PER_PAGE = 5
 
 _SEARCH_URL = "http://editorialcartoonists.com/cartoon/browse.cfm"
-_SEARCH_PAYLOAD = {"membertype": "Regular",
-                   "cartoonist": "Regular",
-                   "orderby": "publicationdate",
-                   "sortorder": "desc",
-                   "submit": "Search"}
+_SEARCH_PAYLOAD = {
+    "membertype": "Regular",
+    "cartoonist": "Regular",
+    "orderby": "publicationdate",
+    "sortorder": "desc",
+    "submit": "Search"
+}
 
-_CARTOON_URL_REGEX = re.compile(r'/cartoon/display.cfm/[0-9]*/')
+_CARTOON_URL_REGEX = re.compile(r'/cartoon/display.cfm/([0-9]*)/')
 _TOTAL_COUNT_URL_REGEX = re.compile(r'totalcount')
 _TOTAL_COUNT_PARAM_REGEX = re.compile(r'totalcount=([0-9]*)')
 
 
-def parse_cartoon_urls(soup: BeautifulSoup):
+def search_request(date_from: str, offset: int = 1) -> BeautifulSoup:
+    assert offset >= 1
+    payload = {**_SEARCH_PAYLOAD, **{"datefrom": date_from, "count": offset}}
+    r = requests.get(_SEARCH_URL, payload)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
+
+
+def parse_cartoon_ids(soup: BeautifulSoup) -> int:
     soup.find_all('a')
     cartoon_urls = soup.find_all('a', href=_CARTOON_URL_REGEX)
     return [a['href'] for a in cartoon_urls]
 
 
-def parse_total_count(soup: BeautifulSoup):
+def parse_total_count(soup: BeautifulSoup) -> int:
     total_count_url = soup.find('a', href=_TOTAL_COUNT_URL_REGEX)
-    return int(_TOTAL_COUNT_PARAM_REGEX.search(total_count_url['href']).groups()[0])
+    groups = _TOTAL_COUNT_PARAM_REGEX.search(total_count_url['href']).groups()
+    return int(groups[0]) if len(groups) > 0 else 0
 
 
 def parse():
-    cartoon_urls = []
+    cartoon_ids = set()
 
     for year in range(2019, 2020):
         date_from = str(year) + "-01-01"
 
-        i = 1
-        total_count = 1
-        while i <= total_count:
-            payload = {**_SEARCH_PAYLOAD, **{"datefrom": date_from, "count": i}}
-            r = requests.get(_SEARCH_URL, payload)
+        soup = search_request(date_from)
+        total_count = parse_total_count(soup)
 
-            soup = BeautifulSoup(r.content, 'html.parser')
-            cartoon_urls.append(parse_cartoon_urls(soup))
-            total_count = parse_total_count(soup)
+        if total_count == 0:
+            return
 
-            # 5 cartoon urls per page
-            i += 5
+        for offset in tqdm(range(1, total_count, _CARTOONS_PER_PAGE)):
+            soup = search_request(date_from, offset)
+            cartoon_ids.add(parse_cartoon_ids(soup))
 
-    print(cartoon_urls)
+    print(cartoon_ids)
 
 
-parse()
+if __name__ == "__main__":
+    parse()
