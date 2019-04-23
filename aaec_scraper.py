90ae8b470ad444a50474ca99c15bd3a8bb3a2ec4
Sam Gehman <sgehman@cs.uw.edu>
2019-04-23 00:34:27 -0700

    Add more scraping logic

--- aaec_scraper.py
+++ aaec_scraper.py
@@ -8,8 +8,12 @@ from bs4 import BeautifulSoup
 import re
 from tqdm import tqdm
 
+_OUT_FILE = "cartoon_ids.txt"
+
 # 5 cartoon urls per search page
 _CARTOONS_PER_PAGE = 5
+_START_YEAR = 1996
+_END_YEAR = 2019
 
 _SEARCH_URL = "http://editorialcartoonists.com/cartoon/browse.cfm"
 _SEARCH_PAYLOAD = {
@@ -33,35 +37,40 @@ def search_request(date_from: str, offset: int = 1) -> BeautifulSoup:
     return soup
 
 
-def parse_cartoon_ids(soup: BeautifulSoup) -> int:
+def parse_cartoon_ids(soup: BeautifulSoup) -> [int]:
     soup.find_all('a')
     cartoon_urls = soup.find_all('a', href=_CARTOON_URL_REGEX)
-    return [a['href'] for a in cartoon_urls]
+    return (int(_CARTOON_URL_REGEX.search(a['href']).groups()[0])
+            for a in cartoon_urls)
 
 
 def parse_total_count(soup: BeautifulSoup) -> int:
     total_count_url = soup.find('a', href=_TOTAL_COUNT_URL_REGEX)
+    if not total_count_url:
+        return 0
     groups = _TOTAL_COUNT_PARAM_REGEX.search(total_count_url['href']).groups()
     return int(groups[0]) if len(groups) > 0 else 0
 
 
 def parse():
-    cartoon_ids = set()
-
-    for year in range(2019, 2020):
+    for year in range(_START_YEAR, _END_YEAR + 1):
+        cartoon_ids = set()
         date_from = str(year) + "-01-01"
 
         soup = search_request(date_from)
         total_count = parse_total_count(soup)
 
         if total_count == 0:
-            return
+            continue
 
+        tqdm.write("Getting cartoons ids for year %i" % year)
         for offset in tqdm(range(1, total_count, _CARTOONS_PER_PAGE)):
             soup = search_request(date_from, offset)
-            cartoon_ids.add(parse_cartoon_ids(soup))
+            cartoon_ids.update(parse_cartoon_ids(soup))
 
-    print(cartoon_ids)
+        with open(_OUT_FILE, 'a') as f:
+            print(f"# {year}", file=f)
+            print(*cartoon_ids, sep='\n', file=f)
 
 
 if __name__ == "__main__":
