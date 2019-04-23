0f1b9ca256980520ceb561557211475c1b6e3e1d
Sam Gehman <sgehman@cs.uw.edu>
2019-04-23 14:53:53 -0700

    Add parser for metadata

--- aaec_image_scraper.py
+++ aaec_image_scraper.py
@@ -4,12 +4,14 @@
 # &totalcount=0&submit=Search
 
 import requests
+from typing import Optional
 from bs4 import BeautifulSoup
 import re
 from tqdm import tqdm
 
 _IN_FILE = "cartoon_ids.txt"
 _BASE_URL = "http://editorialcartoonists.com/cartoon/display.cfm/"
+_METADATA_HEADERS = ("Cartoon Title", "Keywords", "Caption")
 
 
 def cartoon_request(cid: int) -> BeautifulSoup:
@@ -19,9 +21,15 @@ def cartoon_request(cid: int) -> BeautifulSoup:
     return soup
 
 
-def parse_metadata(soup: BeautifulSoup) -> dict:
-
-    return {}
+def parse_metadata(soup: BeautifulSoup) -> Optional[dict]:
+    image_url = soup.find("img", {"name": "Toon"})["src"]
+    title, keywords, caption = (soup.find(text=h + ":")
+                                    .find_parent()
+                                    .next_sibling
+                                    .strip()
+                                for h in _METADATA_HEADERS)
+    keywords = keywords.split(", ")
+    return {"title": title, "keywords": keywords, "caption": caption, "image_url": image_url}
 
 
 def parse():
