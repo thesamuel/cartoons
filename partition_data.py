af746b86fbfedacf2d0c1a9b84b63472f3d31813
Sam Gehman <sgehman@cs.uw.edu>
2019-04-25 01:40:43 -0700

    Add train and val sets

--- partition_data.py
+++ partition_data.py
@@ -1,3 +1,4 @@
+import random
 import json
 import os
 from pathlib import Path
@@ -6,36 +7,53 @@ from urllib.parse import urlparse
 from shutil import copyfile
 from tqdm import tqdm
 
-_DATA_DIR = Path('data')
-_SORTED_DATA_DIR = Path('data-sorted')
+_DATA_DIR = Path('./data')
+_SORTED_DATA_DIR = Path('./data-sorted')
 
-# Get all of the available tags
-tags = {}
-for filename in tqdm(os.listdir(_DATA_DIR)):
+filenames = set(os.listdir(_DATA_DIR))
+downloaded_ids = list(set(int(filename.split(os.extsep)[0])
+                          for filename in os.listdir(_DATA_DIR)))
+
+# Split the data
+random.seed(1996)
+random.shuffle(downloaded_ids)
+
+split = int(0.8 * len(downloaded_ids))
+train_ids = set(downloaded_ids[:split])
+val_ids = set(downloaded_ids[split:])
+
+for cid in tqdm(downloaded_ids):
     try:
-        parts = filename.split(os.extsep)
-        cid = parts[0]
-        ext = parts[-1]
+        image_filename = f"{cid}.jpg"
 
-        local_image_filename = cid + ".jpg"
+        json_filename = f"{cid}.json"
+        if json_filename not in filenames:
+            json_filename = f"{cid}.v2.json"
 
-        if ext != 'json':
-            continue
-
-        with open(_DATA_DIR / filename, 'r') as f:
+        with open(_DATA_DIR / json_filename, 'r') as f:
             metadata = json.load(f)
             url = urlparse(metadata["image_url"])
-            author, year, image_filename = url.path.split(os.sep)[2:]
+            author, year, web_image_filename = url.path.split(os.sep)[2:]
             year = int(year)
 
             # Parse date from image filename
             # FIXME: some usernames end in a "1", so this regex doesn't work
-            date = re.search(r'[0-9]{8}', image_filename)[0]
+            date = re.search(r'[0-9]{8}', web_image_filename)[0]
             year_2, month, day = int(date[:4]), int(date[4:6]), int(date[6:])
-            # assert year == year_2
 
-            os.makedirs(_SORTED_DATA_DIR / author, exist_ok=True)
-            copyfile(_DATA_DIR / local_image_filename, _SORTED_DATA_DIR / author / local_image_filename)
-    except:
-        print("error parsing", filename)
+            # Add train or val to path
+            sorted_path = _SORTED_DATA_DIR
+            if cid in train_ids:
+                sorted_path = sorted_path / "train"
+            elif cid in val_ids:
+                sorted_path = sorted_path / "val"
+            else:
+                raise Exception()
+
+            sorted_path = sorted_path / author
+            os.makedirs(sorted_path, exist_ok=True)
+            copyfile(_DATA_DIR / image_filename, sorted_path / image_filename)
+    except Exception as e:
+        print(e)
+        print("error parsing", json_filename)
         continue
