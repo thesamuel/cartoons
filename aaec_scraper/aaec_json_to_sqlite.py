5b88f046cbdd08b5f1c9045d19fdde7c15b105ca
Sam Gehman <sgehman@cs.uw.edu>
2019-04-29 13:11:28 -0700

    Update sqlite code

--- aaec_scraper/aaec_json_to_sqlite.py
+++ aaec_scraper/aaec_json_to_sqlite.py
@@ -6,22 +6,24 @@ from pathlib import Path
 _DATA_DIR = Path('data') / 'clean-data'
 
 conn = sqlite3.connect("cartoons.sqlite")
-with conn.cursor() as c:
-    # Create table
-    c.execute('''CREATE TABLE Cartoons 
-                 (title text, keywords text, caption text, image_url text, author text, date text)''')
+c = conn.cursor()
 
-    for filename in os.listdir(_DATA_DIR):
-        cid, ext = os.path.splitext(filename)
+# Create table
+c.execute('''CREATE TABLE Cartoons 
+             (title text, keywords text, caption text, image_url text, author text, date text)''')
 
-        if ext != '.json':
-            continue
+for filename in os.listdir(_DATA_DIR):
+    cid, ext = os.path.splitext(filename)
 
-        with open(_DATA_DIR / filename, 'r') as f:
-            metadata = json.load(f)
-            values = (metadata["title"], metadata["keywords"], metadata["caption"],
-                      metadata["image_url"], metadata["author"], metadata["date"])
-            c.execute("INSERT INTO Cartoons VALUES (?, ?, ?, ?, ?, ?)", values)
+    if ext != '.json':
+        continue
 
-    # Save changes
-    conn.commit()
+    with open(_DATA_DIR / filename, 'r') as f:
+        metadata = json.load(f)
+        values = (metadata["title"], metadata["keywords"], metadata["caption"],
+                  metadata["image_url"], metadata["author"], metadata["date"])
+        c.execute("INSERT INTO Cartoons VALUES (?, ?, ?, ?, ?, ?)", values)
+
+# Save changes and exit
+conn.commit()
+conn.close()
