1b20f3d235da4b13b6fd4a6c292a0e1178c7d4a6
Sam Gehman <sgehman@cs.uw.edu>
2019-04-29 12:21:20 -0700

    Count unsplittable and empty in top_k script

--- top_k_classes.py
+++ top_k_classes.py
@@ -3,13 +3,18 @@ import os
 from pathlib import Path
 from collections import Counter
 
-_DATA_DIR = Path('./scraped-data')
-_TOP_K = 1000
+_DATA_DIR = Path('./raw-data')
+_TOP_K = 10
 
 
 def split_keywords(keywords: str) -> list:
     return [t.strip().lower() for t in keywords.split(',')]
 
+
+# Keep statistics for keywords
+unsplittable = 0
+empty = 0
+total = 0
 
 # Get all of the available tags
 tags = {}
@@ -19,14 +24,21 @@ for filename in os.listdir(_DATA_DIR):
     if ext != '.json':
         continue
 
+    total += 1
+
     with open(_DATA_DIR / filename, 'r') as f:
         metadata = json.load(f)
         keywords = metadata['keywords']
-        if keywords:
+        if not keywords:
+            empty += 1
+        elif "," not in keywords:
+            unsplittable += 1
+        else:
             tags[cid] = split_keywords(keywords)
 
-            if "," not in keywords:
-                print(keywords)
+print(f"Keywords found for {total - unsplittable - empty} cartoons:")
+print(f"{unsplittable} unsplittable, {empty} empty")
+print()
 
 # See what the most common tags are
 all_tags = [t for tags_for_cartoon in tags.values() for t in tags_for_cartoon]
