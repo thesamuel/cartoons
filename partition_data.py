c43ee0b44959af048adf274047bd2669dbd55292
Sam Gehman <sgehman@cs.uw.edu>
2019-04-29 01:41:39 -0700

    Update partition_data for obama/trump experiment

--- partition_data.py
+++ partition_data.py
@@ -1,59 +1,64 @@
+from typing import Optional
 import random
 import json
 import os
 from pathlib import Path
-import re
-from urllib.parse import urlparse
 from shutil import copyfile
 from tqdm import tqdm
 
-_DATA_DIR = Path('./data')
-_SORTED_DATA_DIR = Path('./data-sorted')
+_IN_DATA_DIR = Path('./raw-data')
+_OUT_DATA_DIR = Path('./data')
+_RANDOM_SEED = 2019
 
-filenames = set(os.listdir(_DATA_DIR))
-downloaded_ids = list(set(int(filename.split(os.extsep)[0])
-                          for filename in os.listdir(_DATA_DIR)))
+
+def get_tag(metadata: dict) -> Optional[str]:
+    if not metadata["keywords"]:
+        return None
+
+    keywords = metadata["keywords"].lower()
+    if "obama" in keywords:
+        return "obama"
+    elif "trump" in keywords:
+        return "trump"
+    else:
+        return None
+
+
+# Get filenames from directory
+filenames = set(os.listdir(_IN_DATA_DIR))
+ids = list(set(int(filename.split(os.extsep)[0]) for filename in filenames))
+
+# Shuffle the data
+random.seed(_RANDOM_SEED)
+random.shuffle(ids)
 
 # Split the data
-random.seed(1996)
-random.shuffle(downloaded_ids)
+split_1 = int(0.8 * len(ids))
+split_2 = int(0.9 * len(ids))
+train_ids = set(ids[:split_1])
+val_ids = set(ids[split_1:split_2])
+test_ids = set(ids[split_2:])
 
-split = int(0.8 * len(downloaded_ids))
-train_ids = set(downloaded_ids[:split])
-val_ids = set(downloaded_ids[split:])
+# Copy data to output path in PyTorch's ImageFolder structure
+for cid in tqdm(ids):
+    image_filename = f"{cid}.jpg"
+    json_filename = f"{cid}.json"
 
-for cid in tqdm(downloaded_ids):
-    try:
-        image_filename = f"{cid}.jpg"
+    with open(_IN_DATA_DIR / json_filename, 'r') as f:
+        metadata = json.load(f)
 
-        json_filename = f"{cid}.json"
-        if json_filename not in filenames:
-            json_filename = f"{cid}.v2.json"
+        tag = get_tag(metadata)
+        if not tag:
+            continue
 
-        with open(_DATA_DIR / json_filename, 'r') as f:
-            metadata = json.load(f)
-            url = urlparse(metadata["image_url"])
-            author, year, web_image_filename = url.path.split(os.sep)[2:]
-            year = int(year)
+        # Add train or val to path
+        if cid in train_ids:
+            out_path = _OUT_DATA_DIR / "train" / tag
+        elif cid in val_ids:
+            out_path = _OUT_DATA_DIR / "val" / tag
+        else:
+            assert cid in test_ids
+            out_path = _OUT_DATA_DIR / "test" / tag
 
-            # Parse date from image filename
-            # FIXME: some usernames end in a "1", so this regex doesn't work
-            date = re.search(r'[0-9]{8}', web_image_filename)[0]
-            year_2, month, day = int(date[:4]), int(date[4:6]), int(date[6:])
-
-            # Add train or val to path
-            sorted_path = _SORTED_DATA_DIR
-            if cid in train_ids:
-                sorted_path = sorted_path / "train"
-            elif cid in val_ids:
-                sorted_path = sorted_path / "val"
-            else:
-                raise Exception()
-
-            sorted_path = sorted_path / author
-            os.makedirs(sorted_path, exist_ok=True)
-            copyfile(_DATA_DIR / image_filename, sorted_path / image_filename)
-    except Exception as e:
-        print(e)
-        print("error parsing", json_filename)
-        continue
+        os.makedirs(out_path, exist_ok=True)
+        copyfile(_IN_DATA_DIR / image_filename, out_path / image_filename)
