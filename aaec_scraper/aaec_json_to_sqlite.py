16d91c1c8dafc5414dfa7143500e347c69e2da36
Sam Gehman <sgehman@cs.uw.edu>
2019-04-29 15:22:11 -0700

    Update sqlite generator to include an id

--- aaec_scraper/aaec_json_to_sqlite.py
+++ aaec_scraper/aaec_json_to_sqlite.py
@@ -10,7 +10,7 @@ c = conn.cursor()
 
 # Create table
 c.execute('''CREATE TABLE Cartoons 
-             (title text, keywords text, caption text, image_url text, author text, date text)''')
+             (id INT PRIMARY KEY, title TEXT, keywords TEXT, caption TEXT, image_url TEXT, author TEXT, date TEXT)''')
 
 for filename in os.listdir(_DATA_DIR):
     cid, ext = os.path.splitext(filename)
@@ -20,9 +20,9 @@ for filename in os.listdir(_DATA_DIR):
 
     with open(_DATA_DIR / filename, 'r') as f:
         metadata = json.load(f)
-        values = (metadata["title"], metadata["keywords"], metadata["caption"],
+        values = (int(cid), metadata["title"], metadata["keywords"], metadata["caption"],
                   metadata["image_url"], metadata["author"], metadata["date"])
-        c.execute("INSERT INTO Cartoons VALUES (?, ?, ?, ?, ?, ?)", values)
+        c.execute("INSERT INTO Cartoons VALUES (?, ?, ?, ?, ?, ?, ?)", values)
 
 # Save changes and exit
 conn.commit()
