968a32b3e6336158d3eb1a37ccae9fcd21260417
Sam Gehman <sgehman@cs.uw.edu>
2019-05-09 13:36:16 -0700

    Make partition_data into a method

--- partition_data.py
+++ partition_data.py
@@ -7,7 +7,6 @@ from shutil import copyfile
 from tqdm import tqdm
 
 _IN_DATA_DIR = Path('./data/clean-data')
-_OUT_DATA_DIR = Path('./data/classifier-data')
 _RANDOM_SEED = 2019
 
 
@@ -24,41 +23,49 @@ def get_tag(metadata: dict) -> Optional[str]:
         return None
 
 
-# Get filenames from directory
-filenames = set(os.listdir(_IN_DATA_DIR))
-ids = list(set(int(filename.split(os.extsep)[0]) for filename in filenames))
+def partition_data(in_data_dir, out_data_dir):
+    if not os.path.exists(in_data_dir):
+        raise NotADirectoryError(in_data_dir, "doesn't exist")
 
-# Shuffle the data
-random.seed(_RANDOM_SEED)
-random.shuffle(ids)
+    if os.path.exists(out_data_dir):
+        raise IsADirectoryError(out_data_dir, "already exists")
 
-# Split the data
-split_1 = int(0.8 * len(ids))
-split_2 = int(0.9 * len(ids))
-train_ids = set(ids[:split_1])
-val_ids = set(ids[split_1:split_2])
-test_ids = set(ids[split_2:])
+    # Get filenames from directory
+    filenames = set(os.listdir(in_data_dir))
+    ids = list(set(int(filename.split(os.extsep)[0]) for filename in filenames))
 
-# Copy data to output path in PyTorch's ImageFolder structure
-for cid in tqdm(ids):
-    image_filename = f"{cid}.jpg"
-    json_filename = f"{cid}.json"
+    # Shuffle the data
+    random.seed(_RANDOM_SEED)
+    random.shuffle(ids)
 
-    with open(_IN_DATA_DIR / json_filename, 'r') as f:
-        metadata = json.load(f)
+    # Split the data
+    split_1 = int(0.8 * len(ids))
+    split_2 = int(0.9 * len(ids))
+    train_ids = set(ids[:split_1])
+    val_ids = set(ids[split_1:split_2])
+    test_ids = set(ids[split_2:])
 
-        tag = get_tag(metadata)
-        if not tag:
-            continue
+    # Copy data to output path in PyTorch's ImageFolder structure
+    for cid in tqdm(ids):
+        image_filename = f"{cid}.jpg"
+        json_filename = f"{cid}.json"
 
-        # Add train or val to path
-        if cid in train_ids:
-            out_path = _OUT_DATA_DIR / "train" / tag
-        elif cid in val_ids:
-            out_path = _OUT_DATA_DIR / "val" / tag
-        else:
-            assert cid in test_ids
-            out_path = _OUT_DATA_DIR / "test" / tag
+        with open(in_data_dir / json_filename, 'r') as f:
+            metadata = json.load(f)
 
-        os.makedirs(out_path, exist_ok=True)
-        copyfile(_IN_DATA_DIR / image_filename, out_path / image_filename)
+            tag = get_tag(metadata)
+            if not tag:
+                continue
+
+            # Add train or val to path
+            if cid in train_ids:
+                out_path = out_data_dir / "train" / tag
+            elif cid in val_ids:
+                out_path = out_data_dir / "val" / tag
+            elif cid in test_ids:
+                out_path = out_data_dir / "test" / tag
+            else:
+                raise RuntimeError()
+
+            os.makedirs(out_path, exist_ok=True)
+            copyfile(in_data_dir / image_filename, out_path / image_filename)
