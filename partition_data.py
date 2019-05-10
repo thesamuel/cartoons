0518f9c36ba7c1d52f4432abc2eb7424760fc203
Sam Gehman <sgehman@cs.uw.edu>
2019-05-09 19:32:21 -0700

    add class balancing

--- partition_data.py
+++ partition_data.py
@@ -5,30 +5,43 @@ import os
 from pathlib import Path
 from shutil import copyfile
 from tqdm import tqdm
+from collections import defaultdict
 
 _RANDOM_SEED = 2019
 IN_DATA_DIR = './data/clean-data'
 
 
-def get_tag(metadata: dict) -> Optional[str]:
-    if not metadata["keywords"]:
-        return None
+def obama_trump_tag(metadata: dict) -> Optional[str]:
+    all_tags = ""
+    if metadata["keywords"]:
+        all_tags += metadata["keywords"] + " "
 
-    keywords = metadata["keywords"].lower()
-    if "obama" in keywords:
+    if metadata["title"]:
+        all_tags += metadata["title"] + " "
+
+    if metadata["caption"]:
+        all_tags += metadata["caption"]
+
+    all_tags = all_tags.lower()
+    has_obama = "obama" in all_tags
+    has_trump = "trump" in all_tags
+
+    if has_obama and has_trump:
+        return None
+    elif has_obama:
         return "obama"
-    elif "trump" in keywords:
+    elif has_trump:
         return "trump"
     else:
         return None
 
 
-def partition_data(in_data_dir: str, out_data_dir: str):
+def partition_data(in_data_dir: str, out_data_dir: str, tag_func, balance_train: bool = False):
     if not os.path.exists(in_data_dir):
-        raise NotADirectoryError(in_data_dir, "doesn't exist")
+        raise NotADirectoryError(f"{in_data_dir} doesn't exist")
 
     if os.path.exists(out_data_dir):
-        raise IsADirectoryError(out_data_dir, "already exists")
+        raise IsADirectoryError(f"{out_data_dir} already exists")
 
     in_data_dir = Path(in_data_dir)
     out_data_dir = Path(out_data_dir)
@@ -48,27 +61,48 @@ def partition_data(in_data_dir: str, out_data_dir: str):
     val_ids = set(ids[split_1:split_2])
     test_ids = set(ids[split_2:])
 
+    # Keep track of classes for data balancing
+    image_filenames_for_tag = defaultdict(list)
+
     # Copy data to output path in PyTorch's ImageFolder structure
     for cid in tqdm(ids):
         image_filename = f"{cid}.jpg"
         json_filename = f"{cid}.json"
 
-        with open(in_data_dir / json_filename, 'r') as f:
-            metadata = json.load(f)
+        metadata = json.load(open(in_data_dir / json_filename))
 
-            tag = get_tag(metadata)
-            if not tag:
+        tag = tag_func(metadata)
+        if not tag:
+            continue
+
+        # Add train or val to path
+        if cid in train_ids:
+            if balance_train:
+                image_filenames_for_tag[tag].append(image_filename)
                 continue
 
-            # Add train or val to path
-            if cid in train_ids:
-                out_class_dir = out_data_dir / "train" / tag
-            elif cid in val_ids:
-                out_class_dir = out_data_dir / "val" / tag
-            elif cid in test_ids:
-                out_class_dir = out_data_dir / "test" / tag
-            else:
-                raise RuntimeError()
+            out_class_dir = out_data_dir / "train" / tag
+        elif cid in val_ids:
+            # copy_operations["val"][tag] = image_filename
+            out_class_dir = out_data_dir / "val" / tag
+        elif cid in test_ids:
+            # copy_operations["test"][tag] = image_filename
+            out_class_dir = out_data_dir / "test" / tag
+        else:
+            raise RuntimeError()
 
-            os.makedirs(out_class_dir, exist_ok=True)
-            copyfile(in_data_dir / image_filename, out_class_dir / image_filename)
+        os.makedirs(out_class_dir, exist_ok=True)
+        copyfile(in_data_dir / image_filename, out_class_dir / image_filename)
+
+    # Additional step for copying train files in a balanced way
+    if balance_train:
+        min_train_class_len = min([len(c) for c in image_filenames_for_tag.values()])
+
+        for tag in image_filenames_for_tag:
+            for i, image_filename in enumerate(image_filenames_for_tag[tag]):
+                if i >= min_train_class_len:
+                    break
+
+                out_class_dir = out_data_dir / "train" / tag
+                os.makedirs(out_class_dir, exist_ok=True)
+                copyfile(in_data_dir / image_filename, out_class_dir / image_filename)
