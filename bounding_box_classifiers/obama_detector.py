f884614e6775c4d0db661f3104116f1451321847
Sam Gehman <sgehman@cs.uw.edu>
2019-05-13 13:26:25 -0700

    Finish basic obama_detector

--- bounding_box_classifiers/obama_detector.py
+++ bounding_box_classifiers/obama_detector.py
@@ -2,50 +2,74 @@ import argparse
 from pathlib import Path
 
 import torch
-import torch.functional as F
 import torch.nn as nn
+import torch.functional as F
 from torch.optim.adam import Adam
 from torch.utils.data.dataloader import DataLoader
 from torchvision import datasets, transforms
-from tqdm import trange
+from tqdm import tqdm, trange
 
 
 class BasicConvNet(nn.Module):
     def __init__(self, num_classes=2):
         super().__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(7 * 7 * 32, num_classes)
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc = nn.Linear(7 * 7 * 128, num_classes)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
+        out = self.layer3(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
-        return out
+        return F.softmax(out, dim=1)
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, criterion, epoch, log_interval):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = criterion(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
+        if batch_idx % log_interval == 0:
+            tqdm.write(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)}'
+                       f' ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}')
+
+
+def test(model, device, test_loader, criterion):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item() * data.size(0)  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    tqdm.write(f'\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)}'
+               f' ({100. * correct / len(test_loader.dataset):.0f}%)\n')
 
 
 def main():
@@ -69,14 +93,13 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
 
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
     data_dir = Path(args.data_dir)
@@ -91,7 +114,7 @@ def main():
                              ])),
         batch_size=args.batch_size, shuffle=True
     )
-    val_loader = DataLoader(
+    test_loader = DataLoader(
         datasets.ImageFolder(data_dir / "val",
                              transform=transforms.Compose([
                                  transforms.Resize(input_size),
@@ -104,9 +127,11 @@ def main():
 
     model = BasicConvNet(num_classes=2).to(device)
     optimizer = Adam(model.parameters(), lr=args.lr)
+    criterion = nn.CrossEntropyLoss()
 
     for epoch in trange(1, args.epochs + 1):
-        train()
+        train(model, device, train_loader, optimizer, criterion, epoch, args.log_interval)
+        test(model, device, test_loader, criterion)
 
     if args.save_model:
         torch.save(model.state_dict(), f"basic-cnn-lr-{args.lr}-momentum-{args.momentum}")
