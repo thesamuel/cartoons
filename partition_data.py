9bc58de55a4cb744a698b77b2ef116339741c21c
Sam Gehman <sgehman@cs.uw.edu>
2019-05-14 01:38:01 -0700

    Partition data using sql

--- partition_data.py
+++ partition_data.py
@@ -1,110 +1,65 @@
-from typing import Optional
-import random
-import json
 import os
+import sqlite3
 from pathlib import Path
 from shutil import copyfile
-from tqdm import tqdm
-from collections import defaultdict
 
-_RANDOM_SEED = 2019
-SPLIT = 0.8
-IN_DATA_DIR = './data/clean-data'
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
-def obama_trump_tag(metadata: dict) -> Optional[str]:
-    all_tags = ""
-    if metadata["keywords"]:
-        all_tags += metadata["keywords"] + " "
-
-    if metadata["title"]:
-        all_tags += metadata["title"] + " "
-
-    if metadata["caption"]:
-        all_tags += metadata["caption"]
-
-    all_tags = all_tags.lower()
-    has_obama = "obama" in all_tags
-    has_trump = "trump" in all_tags
-
-    if has_obama and has_trump:
-        return None
-    elif has_obama:
-        return "obama"
-    elif has_trump:
-        return "trump"
-    else:
-        return None
-
-
-def balance_data(ids: list, in_data_dir, tag_func) -> list:
-    cids_for_tag = defaultdict(list)
-    for cid in ids:
-        json_filename = f"{cid}.json"
-        metadata = json.load(open(in_data_dir / json_filename))
-        tag = tag_func(metadata)
-        cids_for_tag[tag].append(cid)
-
-    balanced_ids = []
-    min_train_class_len = min([len(c) for c in cids_for_tag.values()])
-    for tag in cids_for_tag:
-        for i, cid in enumerate(cids_for_tag[tag]):
-            if i >= min_train_class_len:
-                break
-            balanced_ids.append(cid)
-
-    return balanced_ids
-
-
-def partition_data(in_data_dir: str, out_data_dir: str, tag_func, balance: bool = False):
+def partition_data(in_data_dir: str, out_data_dir: str, ids: dict, test_size: float = 0.2, balance: bool = False):
+    # Check current directory structure
     if not os.path.exists(in_data_dir):
         raise NotADirectoryError(f"{in_data_dir} does not exist")
-
     if os.path.exists(out_data_dir):
         raise IsADirectoryError(f"{out_data_dir} already exists")
-
     in_data_dir = Path(in_data_dir)
     out_data_dir = Path(out_data_dir)
 
-    # Get filenames from directory
-    filenames = set(os.listdir(in_data_dir))
-    ids = list(set(int(filename.split(os.extsep)[0]) for filename in filenames))
-
-    # Shuffle the data
-    random.seed(_RANDOM_SEED)
-    random.shuffle(ids)
-
     # Balance data
     if balance:
-        ids = balance_data(ids, in_data_dir, tag_func)
-        random.shuffle(ids)
+        min_len = min([len(c) for c in ids.values()])
+        ids = {k: np.random.choice(v, min_len, replace=False) for k, v in ids.items()}
 
-    # Split the data
-    split_idx = int(SPLIT * len(ids))
-    train_ids = set(ids[:split_idx])
-    val_ids = set(ids[split_idx:])
+    # Split data
+    ids_split = train_test_split(*ids.values(), test_size=test_size, shuffle=True)
+    ids_split = np.reshape(ids_split, (-1, 2)).T
+    assert ids_split.shape[0] == 2
+    ids_split = {stage: data for stage, data in zip(["train", "val"], ids_split.T)}
 
     # Copy data to output path in PyTorch's ImageFolder structure
-    for cid in tqdm(ids):
-        image_filename = f"{cid}.jpg"
-        json_filename = f"{cid}.json"
-
-        metadata = json.load(open(in_data_dir / json_filename))
-
-        tag = tag_func(metadata)
-        if not tag:
-            continue
-
-        # Add train or val to path
-        if cid in train_ids:
-            image_folder_out_dir = out_data_dir / "train" / tag
-        elif cid in val_ids:
-            image_folder_out_dir = out_data_dir / "val" / tag
-        else:
-            raise RuntimeError()
-
-        os.makedirs(image_folder_out_dir, exist_ok=True)
-        copyfile(in_data_dir / image_filename, image_folder_out_dir / image_filename)
+    for stage, data in ids_split.items():
+        for tag, cids in zip(ids.keys(), data):
+            image_folder_out_dir = out_data_dir / stage / tag
+            os.makedirs(image_folder_out_dir, exist_ok=False)
+            for cid in cids:
+                image_filename = f"{cid}.jpg"
+                copyfile(in_data_dir / image_filename, image_folder_out_dir / image_filename)
 
 
-partition_data(IN_DATA_DIR, './data/balanced-classifier-data-v3/', tag_func=obama_trump_tag, balance=True)
+def obama_detector_partition():
+    sql_queries = {
+        "obama": """
+        SELECT C.id AS id
+        FROM BoundingBoxes as B
+                 LEFT JOIN
+             Cartoons as C
+             ON B.id = C.id
+        WHERE B.entity LIKE 'Obama';
+        """,
+        "not_obama": """
+        SELECT C.id AS id
+        FROM BoundingBoxes as B
+                 LEFT JOIN
+             Cartoons as C
+             ON B.id = C.id
+        WHERE B.entity IS NULL
+           OR B.entity NOT LIKE 'Obama'
+        """
+    }
+
+    con = sqlite3.connect("cartoons.sqlite")
+    ids = {tag: pd.read_sql_query(query, con)['id'].to_list() for tag, query in sql_queries.items()}
+
+    partition_data('data/clean-data', 'data/bounding-box-obama-detector', ids, test_size=0.2, balance=True)
